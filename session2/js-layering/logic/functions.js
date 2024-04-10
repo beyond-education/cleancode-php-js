cf34c679304820840f235773f2c115ead77aa4be
eMu <emulatoriu@gmail.com>
2024-04-10 10:28:44 +0200

    add function to mix

---
+++ session2/js-layering/logic/functions.js
@@ -0,0 +1,10 @@
+function mixFruitsAndNumbers(fruits, numbers) {
+    const fruitsAndNumbers = [];
+    for(let index = 0; index < fruits.length; index++) {
+        const fruit = fruits[index];
+        const number = numbers[index];
+        const mix = `${number}x ${fruit}`;
+        fruitsAndNumbers.push(mix);
+    }
+    return fruitsAndNumbers;
+}
