cd67e8a6e922e8ae708e4994dfb54aa33de148eb
eMu <emulatoriu@gmail.com>
2024-04-10 11:26:35 +0200

    add heroes

---
+++ session2/php-layering/data/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+$heroes = [
+    new Hero("Superman", 100),
+    new Hero("Bugs Bunny", 80),
+    new Hero("Spiderman", 100),
+    new Hero("John Constantine", 100),
+    new Hero("Aquaman", 50)
+];
